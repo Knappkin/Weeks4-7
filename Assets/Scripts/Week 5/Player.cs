228c8f40ef320621644bddfff4910038831ed774
Knappkin <knappse@shernet.sheridancollege.ca>
2025-02-06 10:24:30 -0500

    Egg reaction now happens when it gets within a certain distance of chicken leg

--- Assets/Scripts/Week 5/Player.cs
+++ Assets/Scripts/Week 5/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 10f;
+    public GameObject reaction;
+    public GameObject item;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        reaction.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,5 +20,14 @@ public class Player : MonoBehaviour
     {
 
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+
+        if((item.transform.position - transform.position).magnitude < 2)
+        {
+            reaction.SetActive(true);
+        }
+        else
+        {
+            reaction.SetActive(false);
+        }
     }
 }
