41049b983e0d9a8a1dc2d7cbe950e5b707e98615
Knappkin <knappse@shernet.sheridancollege.ca>
2025-02-06 11:05:55 -0500

    Finished reaction challenge of coding gym

--- Assets/Scripts/Week 5/Player.cs
+++ Assets/Scripts/Week 5/Player.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 
     public float speed = 10f;
-    public GameObject reaction;
+    public GameObject bubble;
+    public Image reaction;
     public GameObject item;
+    public Sprite[] reactionOptions;
+    bool check = false;
+    bool lastCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        reaction.SetActive(false);
+       bubble.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,11 +28,25 @@ public class Player : MonoBehaviour
 
         if((item.transform.position - transform.position).magnitude < 2)
         {
-            reaction.SetActive(true);
+            check = true;
+            if(check != lastCheck)
+            {
+                setReaction();
+            }
+            bubble.SetActive(true);
         }
         else
         {
-            reaction.SetActive(false);
+            bubble.SetActive(false);
+            check = false;
         }
+
+        lastCheck = check;
+    }
+
+    void setReaction()
+    {
+        reaction.sprite = reactionOptions[Random.Range(0, reactionOptions.Length)];
+
     }
 }
